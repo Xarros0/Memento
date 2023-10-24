8dad56c08f281f5d3022e1a81806f5ab75daaed1
jeremike <103106203+Xarros0@users.noreply.github.com>
2023-10-24 11:11:54 +0300

    first commit

---
+++ src/Memento.java
@@ -0,0 +1,12 @@
+// Memento-luokka, joka sisältää arvotun luvun
+public class Memento {
+    private final int number;
+
+    public Memento(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
